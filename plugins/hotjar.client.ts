3a03b2283e22dfe842bda58637abc14463dc8722
Dennis Maaß <dennis.maass@thing.online>
2023-05-29 19:52:38 +0200

    chore: console logs

--- plugins/hotjar.client.ts
+++ plugins/hotjar.client.ts
@@ -1,10 +1,14 @@
 import VueHotjar from "vue-hotjar-next";
 
 export default defineNuxtPlugin((nuxtApp) => {
+  console.log("process.env.HOTJAR_ID", process.env.HOTJAR_ID);
   if (!process.env.HOTJAR_ID) {
+    console.warn("HOTJAR_ID is not set");
     return;
   }
   const id = Number.parseInt(process.env.HOTJAR_ID);
+  console.log("id", id);
+  console.log("process.env.NODE_ENV", process.env.NODE_ENV);
   nuxtApp.vueApp.use(VueHotjar, {
     id,
     isProduction: process.env.NODE_ENV !== "production",
