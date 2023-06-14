01a7c35e7fd485f84ca9887b6e1babc003f75124
Dennis Maaß <dennis.maass@thing.online>
2023-06-14 21:51:21 +0200

    feat: add filter

--- components/section/StoriesSection.vue
+++ components/section/StoriesSection.vue
@@ -45,11 +45,13 @@ await useAsyncData(
           träume, lerne und wachse mit Bestes-Kinderbuch.de - denn Geschichten sind der Schlüssel zur Fantasie!
         </p>
       </div>
+      <Filter>
       <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <StoryTeaser v-for="story in stories" :key="story.id" :data="story"
           class="flex flex-col items-start justify-between">
         </StoryTeaser>
       </div>
+    </Filter>
     </Container>
   </Section>
 </template>
